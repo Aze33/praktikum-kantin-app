ac5ed24274f2d7dbb205a716d7b4762a0f8705b5
Zahran Zaidan Saputra <zahranzaidan3@gmail.com>
2026-05-06 18:20:54 +0700

    Update app.py data to Dota 2 items

--- backend/app.py
+++ backend/app.py
@@ -5,19 +5,23 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+
 kantin_data = {
-    "menu": ["Nasi Goreng", "Es Teh", "Gorengan"]
+    "menu": ["Blink Dagger", "Aghanim's Scepter", "Monkey King Bar"]
 }
 
 @app.route('/api/info', methods=['GET'])
 def get_info():
     # Menangkap variabel lingkungan dari Kubernetes YAML
-    nama_mahasiswa = os.getenv('NAMA_MAHASISWA', 'Nama Default')
-    nim_mahasiswa = os.getenv('NIM_MAHASISWA', '000000')
+    nama_mahasiswa = os.getenv('NAMA_MAHASISWA', 'Zahran Zaidan Saputra')
+    nim_mahasiswa = os.getenv('NIM_MAHASISWA', '2415429')
     
-    # Memasukkan identitas ke dalam respons
+    # Menangkap nama catalog dari env, jika tidak ada default-nya langsung Dota 2 Catalog
+    nama_kantin = os.getenv('NAMA_KANTIN', 'Dota 2 Item Catalog')
+
+    # Memasukkan identitas ke dalam respons JSON
     response_data = {
-        "nama_kantin": "Kantin FPMIPA",
+        "nama_kantin": nama_kantin,
         "identitas": f"{nama_mahasiswa} - {nim_mahasiswa}",
         "menu": kantin_data["menu"]
     }
@@ -28,7 +32,7 @@ def add_menu():
     new_item = request.json.get('item')
     if new_item:
         kantin_data["menu"].append(new_item)
-        return jsonify({"message": "Menu berhasil ditambah!", "menu": kantin_data["menu"]}), 201
+        return jsonify({"message": "Item berhasil ditambahkan!", "menu": kantin_data["menu"]}), 201
     return jsonify({"error": "Data tidak valid"}), 400
 
 if __name__ == '__main__':
